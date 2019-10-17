07c480212054f2558c9dfc61bddeacdb030ddab0
Edvinas01 <edvinas108@gmail.com>
2019-10-17 11:57:18 +0200

    Initial Arduino code

---
+++ src/main.ino
@@ -0,0 +1,72 @@
+
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+    updates by chegewara
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID    "d2007d50-6730-41c0-a871-06695ae08a55"
+#define READ_CHAR_UUID  "2be82d7d-6dd9-40ee-8ee9-af581af0fc92" // todo not used
+#define WRITE_CHAR_UUID "2be82d7d-6dd9-40ee-8ee9-af581af0fc92"
+
+class ReadCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+      if (value.length() > 0) {
+        Serial.print("New value: ");
+        for (int i = 0; i < value.length(); i++)
+          Serial.print(value[i]);
+
+        Serial.println();
+      }
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("Nope");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  
+  BLECharacteristic *readChar = pService->createCharacteristic(
+    READ_CHAR_UUID,
+    BLECharacteristic::PROPERTY_READ |
+    BLECharacteristic::PROPERTY_WRITE
+  );
+
+  readChar->setValue("Hi there");
+
+  BLECharacteristic *writeChar = pService->createCharacteristic(
+    WRITE_CHAR_UUID,
+    BLECharacteristic::PROPERTY_READ |
+    BLECharacteristic::PROPERTY_WRITE
+  );
+
+  writeChar->setCallbacks(new ReadCallbacks());
+  writeChar->setValue("Hi there again");
+  
+  pService->start();
+  
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+}
